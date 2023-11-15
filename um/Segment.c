067ff8aae38a8d5592acd5326cb0b60ae33f0fba
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-11-14 19:51:36 -0500

    Segment module

--- um/Segment.c
+++ um/Segment.c
@@ -16,7 +16,13 @@ uint32_t Segment_size(Segment seg);
 Word Segment_getWord(Segment seg, uint32_t position);
 void Segment_setWord(Segment seg, uint32_t position, Word word);
 
-
+/*
+ * Name      : Segment_new
+ * Purpose   : Creates new segment for the UM memory
+ * Parameter : (uint32_t) size -- The number of words in the segment
+ * Return    : (Segment) The struct representing a specific segment of memory
+ * Notes     : Will CRE if it does not have enough memory to allocate
+ */
 Segment Segment_new(uint32_t size)
 {
         Segment seg  = ALLOC(sizeof(*seg));
@@ -27,6 +33,14 @@ Segment Segment_new(uint32_t size)
         return seg;
 }
 
+/*
+ * Name      : Segment_copy
+ * Purpose   : Creates a copy of the given segment
+ * Parameter : (Segment) seg -- The segment to copy
+ * Return    : (Segment) The struct representing the copy of the segment
+ * Notes     : Will CRE if the given segment is null;
+ *             Will CRE if it does not have enough memory to allocate
+ */
 Segment Segment_copy(Segment seg)
 {
         assert(seg != NULL);
@@ -41,25 +55,61 @@ Segment Segment_copy(Segment seg)
 
 }
 
+/*
+ * Name      : Segment_free
+ * Purpose   : Frees segment of the UM memory
+ * Parameter : (Segment *) seg -- The pointer to the segment to free
+ * Return    : None
+ * Notes     : Will CRE if seg is null or *seg is null;
+ *             Will set the value in seg to NULL
+ */
 void Segment_free(Segment *seg)
 {
         assert(seg != NULL && *seg != NULL);
         FREE((*seg) -> words);
         FREE(seg);
+
+        *seg = NULL;
 }
 
+/*
+ * Name      : Segment_size
+ * Purpose   : Gets the size of the segment
+ * Parameter : (Segment) seg -- The segment to check the size of 
+ * Return    : (uint32_t) The size of the segment
+ * Notes     : Will CRE if seg is NULL
+ */
 uint32_t Segment_size(Segment seg)
 {
         assert(seg != NULL);
         return seg -> size;
 }
 
+/*
+ * Name      : Segment_getWord
+ * Purpose   : Gets the word at a given position within the segment
+ * Parameter : (Segment)  seg      -- The segment to get word from
+ *             (uint32_t) position -- Position to access
+ * Return    : (Word)     The word that was located at the given position
+ * Notes     : Will CRE if seg is NULL or the position is greater than the 
+ *                 segment size
+ */
 Word Segment_getWord(Segment seg, uint32_t position)
 {
         assert(seg != NULL && position < seg -> size);
         return seg -> words[position];
 }
 
+/*
+ * Name      : Segment_setWord
+ * Purpose   : Sets the word at a given position within the segment
+ * Parameter : (Segment)  seg      -- The segment to set word in
+ *             (uint32_t) position -- Position to access
+ *             (Word)     word     -- Word to place at the position
+ * Return    : None
+ * Notes     : Will CRE if seg is NULL or the position is greater than the 
+ *                 segment size
+ */
 void Segment_setWord(Segment seg, uint32_t position, Word word)
 {
         assert(seg != NULL && position < seg -> size);
