66fa193a7680c4959be07aea658aba0022453cf6
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-11-15 11:21:45 -0500

    makefile + registers.c fixes

--- um/Registers.c
+++ um/Registers.c
@@ -8,9 +8,10 @@
 #include <assert.h>
 #include <mem.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef struct Registers {
-    uint_32 registerArray[8];
+    uint32_t registerArray[8];
 } *Registers;
 
 /*
@@ -21,7 +22,7 @@ typedef struct Registers {
  *  Notes     : Will CRE on failure to allocate memory
  *              
  */
-void Registers_new() 
+Registers Registers_new() 
 {
         Registers newRegisters = ALLOC(sizeof(*newRegisters));
         return newRegisters;
@@ -46,15 +47,15 @@ void Registers_free(Registers *regPointer)
  *  Name      : Registers_setRegisters
  *  Purpose   : Sets a given register within the set
  *  Parameters: (Registers) reg -- Set of registers to update
- *              (uint_32) regID -- Position/ID of the register to access
- *              (uint_32) data -- Data to store in the register
+ *              (uint32_t) regID -- Position/ID of the register to access
+ *              (uint32_t) data -- Data to store in the register
  *  Returns   : (None)
  *  Notes     : Will CRE if the pointer Registers is null;
  *              Will CRE if the data in the pointer is null;
  */
-void Registers_setRegisters(Registers reg, uint_32 regID, uint_32 data)
+void Registers_setRegisters(Registers reg, uint32_t regID, uint32_t data)
 {
-        assert(reg != NULL && *reg != NULL);
+        assert(reg != NULL);
         reg->registerArray[regID] = data;
 }
 
@@ -62,13 +63,13 @@ void Registers_setRegisters(Registers reg, uint_32 regID, uint_32 data)
  *  Name      : Registers_getRegisters
  *  Purpose   : Gets the data stored in given register within the set
  *  Parameters: (Registers) Set of registers
- *              (uint_32) reg -- Position/ID of the register to access
- *  Output    : (uint_32) regID -- Data stored in the accessed registers
+ *              (uint32_t) reg -- Position/ID of the register to access
+ *  Output    : (uint32_t) regID -- Data stored in the accessed registers
  *  Notes     : Will CRE if the pointer Registers is null;
  *              Will CRE if the data in the pointer is null;      
  */
-void Registers_getRegisters(Registers reg, uint_32 regID)
+uint32_t Registers_getRegisters(Registers reg, uint32_t regID)
 {
-        assert(regPointer != NULL && *regPointer != NULL);
-        return reg->registerArray[regId];
+        assert(reg != NULL);
+        return reg->registerArray[regID];
 }
--- um/Registers.h
+++ um/Registers.h
@@ -4,12 +4,13 @@
  *   Purpose    : Module representing the registers of our Universal Machine
  *   Editors    : Matthew Wong (mwong14), Ivi Fung (sfung02)
  */
+#include <stdint.h>
 
 typedef struct Registers *Registers;
 
 void Registers_new();
 void Registers_free(Registers *reg);
-void Registers_setRegisters(Registers reg, uint_32 regID, uint_32 data);
-void Registers_getRegisters(Registers reg, uint_32 regID);
+void Registers_setRegisters(Registers reg, uint32_t regID, uint32_t data);
+void Registers_getRegisters(Registers reg, uint32_t regID);
 
 
