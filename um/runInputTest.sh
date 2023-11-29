04fa1a2da986db3f2049b37c31465f3b10c8d4c7
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-11-28 19:47:32 -0500

    bash script to run + solutions

---
+++ um/runInputTest.sh
@@ -0,0 +1,28 @@
+ #! /bin/sh
+
+# Midmark Testing
+echo "------------"
+echo "Midmark Test"
+echo "------------"
+echo "Timing:"
+/usr/bin/time ./um Tests/midmark.um > /dev/null
+
+# Sandmark Testing
+echo "-------------"
+echo "Sandmark Test"
+echo "-------------"
+echo "Timing:"
+/usr/bin/time ./um Tests/sandmark.umz > /dev/null
+
+
+# Advent Testing
+echo "-----------"
+echo "Advent Test"
+echo "-----------"
+echo "Timing:"
+/usr/bin/time ./um Tests/advent.umz < Tests/adventPartialSolution.txt \
+        > /dev/null
+
+# Instruction count
+echo "Midmark Instruction Count:"
+valgrind --tool=callgrind --dump-instr=yes ./um Tests/midmark.um > /dev/null
